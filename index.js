e120068e85755e26dd6829d6f563d4674480941c
andreaguo <andrea.guo2007@gmail.com>
2022-11-10 14:03:25 -0500

    idekanymore

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-//The Document Object Model, or DOM, is essentially the elements you write into your HTML. DOMSelectors allow you to easily access your HTML in JS.
-
-// DOMSelectors are objects that hold access to the DOM that you can call upon easily at any point in your code.
-
 const DOMSelectors = {
-    button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
-    text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
-    box: document.getElementById("big-black-box"),
-    points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
-    null: document.getElementById("point"), // if you try to access a class with getElementById, it will result in "null"
-    empty: document.querySelector(".nothing"),
-  };
-  
-  function backgroundAndText(background, text) {
-    background.style.backgroundColor = "red"
-    text.textContent = "this is now a big bigger red box";
-    text.style.fontSize = "40px";
-  }
-  console.log(DOMSelectors.box);
-  console.log(DOMSelectors.button);
-  console.log(DOMSelectors.text);
-  console.log(DOMSelectors.points);
-  console.log(DOMSelectors.points[0]); // this returns the first ever element in the document with class 'point'
-  console.log(DOMSelectors.null);
-  console.log(DOMSelectors.nothing);
-  // when you look in the console after logging DOMSelectors, you will be able to see the elements that the DOMSelector accesses.
-  // if you have a DOMSelector for an element that doesn't exist, it will show up as undefined.
+  button: document.getElementById("btn"),
+  box: document.getElementById("container-box"),
+  input: document.querySelector(`.input`),
+};
+
+DOMSelectors.box.insertAdjacentHTML(
+  "beforebegin",
+  `We can add text into an HTML file by writing it in JS!`
+);
+// insertAdjacentHTML will allow you to add HTML elements into your project through the JS. The positioning of this is relative to other DOM elements, and can be positioned around them. This positioning can be done with "beforebegin", "afterbegin", "beforeend", and "afterend".
+
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value; // .value lets you get the values of an element, in this case something you typed in
+
+  console.log(input);
+
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `
+        <p class = 'extra-text'>${input}</p>
+    `
+  );
+}); // this function will cause new HTML to be added when the input button is clicked.
+
+DOMSelectors.button.insertAdjacentHTML(
+  "afterend",
+  `<p>you can have multiple HTML elements in the same DOMSelector!<p>
+  <img src="https://i.imgur.com/zQ5ds03.jpeg" class="cat-poster"></img>`
+);
+// you can put multiple HTML tags in the same DOMSelector as if you were writing out regular HTML, you're also able to assigned classes and ids that can be used for styling the elements you add through js.
