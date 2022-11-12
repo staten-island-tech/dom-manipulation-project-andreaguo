84bda4beadc6c74cc634f64a63fabfc4a81eddd6
andreaguo <113132239+andreaguo@users.noreply.github.com>
2022-11-12 12:28:06 -0500

    foreach

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),           // "Enter" button
   box: document.getElementById("entries"),          // All Entries
-  input: document.querySelector('#nameInput'),     // Input Name responses 
-  // // input2: document.querySelector(`#ingrInput`),     // Input ingrediences responses 
-  // // input3: document.querySelector(`#instrInput`),    // Input instructions responses
-  input4: document.querySelector(`#imgInput`),      // Input image responses
+  input: document.querySelectorAll('#input'),     // Input Name responses 
+  imageURL: document.querySelector(`#imgInput`),      // Input image responses
   clearButton: document.querySelector(`#clear`)     // "Reload" button  
 };
 
-function imageSizing(image) {
-  image.style.height = "15vh";
+function returnInput() {
+let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
+  DOMSelectors.input.value = "";
+}
+
+function style() {
+  input.style.fontSize = "60px";
 }
 
 DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
-  DOMSelectors.input.value = "";
+  returnInput();
 }); // when "enter" button is clicked, name of food will be printed at the bottom
 
 DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input4.value;
+  let input = DOMSelectors.imageURL.value;
   DOMSelectors.box.insertAdjacentHTML("beforeend", `<img src="${input}"></img>`);
-  DOMSelectors.input4.value = "";
+  DOMSelectors.imageURL.value = "";
 }); // when "enter" button is clicked, name of food will be printed at the bottom
 
-DOMSelectors.button.insertAdjacentHTML(
+  DOMSelectors.button.insertAdjacentHTML(
   "afterend",
   `<h2>full entry here: <h2>`
 ); // heading separates results from entries
---
+++ second.js
@@ -0,0 +1,6 @@
+const DOMSelectors = {
+    button: document.getElementById("btn"),
+    text: document.querySelector(".title"),
+    box: document.getElementById("entries"),
+}
+
