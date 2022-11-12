bf271888a267c18e52e4d42be130dff5949ad471
andreaguo <113132239+andreaguo@users.noreply.github.com>
2022-11-11 21:45:13 -0500

    hj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("container-box"),
-  input: document.querySelector(`.input`),
+  input: document.querySelector(`#input`),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
@@ -10,9 +10,8 @@ DOMSelectors.button.addEventListener("click", function () {
   DOMSelectors.input.value = "";
 }); // this function will cause new HTML to be added when the input button is clicked.
 
-// DOMSelectors.button.insertAdjacentHTML(
-//   "afterend",
-//   `<p>you can have multiple HTML elements in the same DOMSelector!<p>
-//   <img src="https://i.imgur.com/zQ5ds03.jpeg" class="cat-poster"></img>`
-// );
-// // you can put multiple HTML tags in the same DOMSelector as if you were writing out regular HTML, you're also able to assigned classes and ids that can be used for styling the elements you add through js.
+DOMSelectors.button.insertAdjacentHTML(
+  "afterend",
+  `<p>you can have multiple HTML elements in the same DOMSelector!<p>`
+);
+// you can put multiple HTML tags in the same DOMSelector as if you were writing out regular HTML, you're also able to assigned classes and ids that can be used for styling the elements you add through js.
