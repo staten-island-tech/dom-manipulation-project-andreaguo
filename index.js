324713bb130d057df6034e2dce63f760e093150a
andreaguo <andrea.guo2007@gmail.com>
2022-11-14 14:04:28 -0500

    xc

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+
 const DOMSelectors = {
   button: document.getElementById("btn"),           // "Enter" button
   box: document.getElementById("entries"),          // All Entries
-  input: document.querySelectorAll('#input'),     // Input Name responses 
-  allInputsArray: Array.from(input),
   imageURL: document.querySelector(`#imgInput`),      // Input image responses
   clearButton: document.querySelector(`#clear`)     // "Reload" button  
 };
 
-function returnResponses() {
-let input = DOMSelectors.allInputsArray.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
-  DOMSelectors.allInputsArray.value = "";
-}
+// function returnResponses() {
+// let input = DOMSelectors.allInputsArray.value;
+//   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
+//   DOMSelectors.allInputsArray.value = "";
+// }
 
-function style() {
-  input.style.fontSize = "60px";
-}
+// function style() {
+//   input.style.fontSize = "60px";
+// }
 
 DOMSelectors.button.addEventListener("click", function () {
-  DOMSelectors.allInputsArray.forEach((input) => {
+  let input = document.querySelectorAll(`#input`);
+  let inputs = Array.from(input);
+  inputs.forEach((element) => {
+    input.insertAdjacentHTML("beforeend", `<p> ${element} </p>`);
+    input.value = "";
+  })
+  /* DOMSelectors.allInputsArray.forEach((input) => {
+    console.log(input)
       DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
       DOMSelectors.input.value = "";
-  });  
+  });   */
 }); // when "enter" button is clicked, name of food will be printed at the bottom
 
 DOMSelectors.button.addEventListener("click", function () {
